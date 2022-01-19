189fbafc77b68584f05bf0969ec1faa6c98a88e7
Robbert Broersma <mail@robbertbroersma.nl>
2022-01-19 12:50:40 +0100

    add some more forms

--- next.config.js
+++ next.config.js
@@ -31,13 +31,4 @@ module.exports = {
       },
     ];
   },
-  async redirects() {
-    return [
-      {
-        source: '/',
-        destination: '/form',
-        permanent: false,
-      },
-    ];
-  },
 };
