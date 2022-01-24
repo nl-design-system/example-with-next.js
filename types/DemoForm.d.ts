8617f74c404d7970806f05ce19f796b56cade89b
Robbert Broersma <mail@robbertbroersma.nl>
2022-01-24 12:57:43 +0100

    add ForeignAddress type

--- types/DemoForm.d.ts
+++ types/DemoForm.d.ts
@@ -2,9 +2,14 @@ import { CommercialDetails } from './CommercialDetails';
 import { ContactDetails } from './ContactDetails';
 import { FinancialDetails } from './FinancialDetails';
 import { PersonalDetails } from './PersonalDetails';
+import { ForeignAddress } from './ForeignAddress';
 
-export interface DemoFormInput extends PersonalDetails, ContactDetails, FinancialDetails, CommercialDetails {}
-export interface DemoFormChecked {
+export interface DemoFormChecked
+  extends PersonalDetails,
+    ContactDetails,
+    FinancialDetails,
+    CommercialDetails,
+    ForeignAddress {
   'accept-data-handling': boolean;
   'subscribe-newsletter': boolean;
 }
---
+++ types/ForeignAddress.d.ts
@@ -0,0 +1,5 @@
+export interface ForeignAddress {
+  'address-line1'?: string;
+  'address-line2'?: string;
+  'address-line3'?: string;
+}
