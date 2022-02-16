ac4cdc0c9a43bdf51b85635dd264fc683a282a07
Robbert Broersma <mail@robbertbroersma.nl>
2022-02-16 14:43:26 +0100

    feat: Utrecht article component with tests

---
+++ src/components/utrecht/Article.test.jsx
@@ -0,0 +1,63 @@
+import { render, screen } from "@testing-library/react";
+import { createRef } from "react";
+import { Article } from "./Article";
+import "@testing-library/jest-dom";
+
+describe("Article", () => {
+  it("renders an article role element", () => {
+    render(<Article>Breaking news</Article>);
+
+    const article = screen.getByRole("article");
+
+    expect(article).toBeInTheDocument();
+    expect(article).toBeVisible();
+  });
+
+  it("renders an article HTML element", () => {
+    const { container } = render(<Article>Secrets</Article>);
+
+    const article = container.querySelector("article");
+
+    expect(article).toBeInTheDocument();
+  });
+
+  it("renders rich text content", () => {
+    render(
+      <Article>
+        <strong>Breaking</strong> news
+      </Article>
+    );
+
+    const article = screen.getByRole("article");
+
+    const richText = article.querySelector("strong");
+
+    expect(richText).toBeInTheDocument();
+  });
+
+  it("can be hidden", () => {
+    const { container } = render(<Article hidden>Secrets</Article>);
+
+    const article = container.querySelector("article");
+
+    expect(article).not.toBeVisible();
+  });
+
+  it("can have a custom class name", () => {
+    render(<Article className="breaking-news">Breaking news</Article>);
+
+    const article = screen.getByRole("article");
+
+    expect(article).toHaveClass("breaking-news");
+  });
+
+  it("supports ForwardRef in React", () => {
+    const ref = createRef();
+
+    render(<Article ref={ref}>Breaking news</Article>);
+
+    const article = screen.getByRole("article");
+
+    expect(ref.current).toBe(article);
+  });
+});
