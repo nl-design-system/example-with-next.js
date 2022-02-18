23e5dff7ca4ed459410209f44be1696f614f0cb9
Robbert Broersma <mail@robbertbroersma.nl>
2022-02-18 18:58:22 +0100

    feat: Utrecht radio button component with tests

---
+++ src/components/utrecht/RadioButton.tsx
@@ -0,0 +1,29 @@
+import clsx from "clsx";
+import { ForwardedRef, forwardRef, InputHTMLAttributes } from "react";
+
+interface RadioButtonProps extends Omit<InputHTMLAttributes<HTMLInputElement>, "type" | "readOnly"> {
+  invalid?: boolean;
+}
+
+export const RadioButton = forwardRef(
+  ({ disabled, invalid, required, className, ...restProps }: RadioButtonProps, ref: ForwardedRef<HTMLInputElement>) => (
+    <input
+      {...restProps}
+      ref={ref}
+      type="radio"
+      className={clsx(
+        "utrecht-radio-button",
+        "utrecht-radio-button--html-input",
+        disabled && "utrecht-radio-button--disabled",
+        invalid && "utrecht-radio-button--invalid",
+        required && "utrecht-radio-button--required",
+        className
+      )}
+      aria-invalid={invalid || undefined}
+      disabled={disabled}
+      required={required}
+    />
+  )
+);
+
+RadioButton.displayName = "utrecht-radio-button";
