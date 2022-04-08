a81904036bf733258f88be26bc14684c07301461
Robbert Broersma <mail@robbertbroersma.nl>
2022-04-08 10:39:58 +0200

    feat: table example 2

---
+++ pages/table-example-2b.tsx
@@ -0,0 +1,63 @@
+import Head from "next/head";
+import { Document } from "../src/components/utrecht/Document";
+import { Heading1 } from "../src/components/utrecht/Heading1";
+import { Heading2 } from "../src/components/utrecht/Heading2";
+import { Page } from "../src/components/utrecht/Page";
+import { PageContent } from "../src/components/utrecht/PageContent";
+import { Paragraph } from "../src/components/utrecht/Paragraph";
+import { Table } from "../src/components/utrecht/Table";
+import { TableBody } from "../src/components/utrecht/TableBody";
+import { TableCell } from "../src/components/utrecht/TableCell";
+import { TableHeaderCell } from "../src/components/utrecht/TableHeaderCell";
+import { TableRow } from "../src/components/utrecht/TableRow";
+
+export default function TableExample2b() {
+  return (
+    <>
+      <Document lang="nl">
+        <Head>
+          <title>Voorbeeld met tabel</title>
+        </Head>
+        <Page>
+          <PageContent>
+            <main>
+              <Heading1>Voorbeeld met tabel</Heading1>
+              <section>
+                <Heading2 id="table-header">Openingstijden</Heading2>
+                <Paragraph>Deze tabel is gemaakt met extra opmaak.</Paragraph>
+                <Paragraph>We zijn alleen geopend op afspraak</Paragraph>
+                <Table aria-labelledby="table-header">
+                  <TableBody>
+                    <TableRow>
+                      <TableHeaderCell>Maandag:</TableHeaderCell>
+                      <TableCell>09.00 tot 17.00 uur</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableHeaderCell>Dinsdag:</TableHeaderCell>
+                      <TableCell>08.00 tot 20.00 uur</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableHeaderCell>Woensdag:</TableHeaderCell>
+                      <TableCell>09.00 tot 17.00 uur</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableHeaderCell>Donderdag:</TableHeaderCell>
+                      <TableCell>12.00 tot 20.00 uur</TableCell>
+                    </TableRow>
+                    <TableRow>
+                      <TableHeaderCell>Vrijdag:</TableHeaderCell>
+                      <TableCell>09.00 tot 17.00 uur</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+                <div>
+                  <a href="#">afspraak maken</a>
+                </div>
+              </section>
+            </main>
+          </PageContent>
+        </Page>
+      </Document>
+    </>
+  );
+}
