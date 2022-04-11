2f34c2d677e49954efc540c966ee88d7b054d32a
Robbert Broersma <mail@robbertbroersma.nl>
2022-04-11 20:04:18 +0200

    feat: datalist example

---
+++ pages/table-example-4b.tsx
@@ -0,0 +1,97 @@
+import Head from "next/head";
+import Link from "next/link";
+import {
+  Document,
+  Heading1,
+  Heading2,
+  Page,
+  PageContent,
+  Paragraph,
+  Separator,
+  Table,
+  TableBody,
+  TableCell,
+  TableHeader,
+  TableHeaderCell,
+  TableRow,
+} from "../src/components";
+
+export default function TableExample4() {
+  return (
+    <>
+      <Document lang="nl">
+        <Head>
+          <title>Voorbeeld met tabel</title>
+        </Head>
+        <Page>
+          <PageContent>
+            <main>
+              <Heading1>Voorbeeld met tabel</Heading1>
+              <section>
+                <Heading2>Zaakdetails</Heading2>
+                <dl>
+                  <div>
+                    <dt>Zaaknummer</dt>
+                    <dd>9919905</dd>
+                  </div>
+                  <div>
+                    <dt>Zaaktype</dt>
+                    <dd>Klacht behandelen</dd>
+                  </div>
+                  <div>
+                    <dt>Omschrijving</dt>
+                    <dd></dd>
+                  </div>
+                  <div>
+                    <dt>Toelichting:</dt>
+                    <dd>tourbillon@example.com</dd>
+                  </div>
+                  <div>
+                    <dt>Registratiedatum:</dt>
+                    <dd>29-03-2022</dd>
+                  </div>
+                </dl>
+                <Heading2>Doorlooptijd</Heading2>
+                <dl>
+                  <div>
+                    <dt>Startdatum</dt>
+                    <dd>30-03-2022</dd>
+                  </div>
+                  <div>
+                    <dt>Geplande einddatum</dt>
+                    <dd>16-05-2022</dd>
+                  </div>
+                  <div>
+                    <dt>Einddatum</dt>
+                    <dd></dd>
+                  </div>
+                </dl>
+                <Paragraph>
+                  <Link href="/">Bekijk de documenten</Link>
+                </Paragraph>
+                <Separator />
+                <Heading2>Statussen</Heading2>
+                <Table>
+                  <TableHeader>
+                    <TableRow>
+                      <TableHeaderCell>Tijdstip</TableHeaderCell>
+                      <TableHeaderCell>Status</TableHeaderCell>
+                      <TableHeaderCell>Toelichting</TableHeaderCell>
+                    </TableRow>
+                  </TableHeader>
+                  <TableBody>
+                    <TableRow>
+                      <TableCell>29-03-2022 16:02:10</TableCell>
+                      <TableCell>Ontvangen</TableCell>
+                      <TableCell>Aanvraaggegevens succesvol verstuurd</TableCell>
+                    </TableRow>
+                  </TableBody>
+                </Table>
+              </section>
+            </main>
+          </PageContent>
+        </Page>
+      </Document>
+    </>
+  );
+}
