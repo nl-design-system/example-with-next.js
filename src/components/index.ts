be876f562a26eb6c6feac957da0dece2a8b79413
Robbert Broersma <mail@robbertbroersma.nl>
2022-04-22 18:28:16 +0200

    refactor: use custom Next.js Link component

--- src/components/index.ts
+++ src/components/index.ts
@@ -9,6 +9,7 @@ export * from '@utrecht/component-library-react';
 // Components for this project:
 export { Input } from './Input';
 export { LanguageToggle } from './LanguageToggle';
+export { Link } from './Link';
 export { ThemeSwitcher } from './ThemeSwitcher';
 export { ValidationMessages } from './ValidationMessages';
 export { ValueCurrency } from './ValueCurrency';
