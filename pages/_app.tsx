17572a38796e1399d4df5f693956be015f7b47b3
Yolijn <yolijn@frameless.io>
2022-05-18 13:07:10 +0200

    build: patch update npm packages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,12 @@ import type { AppProps } from "next/app";
 import { useEffect } from "react";
 import { ThemeProvider } from "../src/context/ThemeContext";
 
-import "@utrecht/design-tokens/dist/theme/index.css";
-import "@utrecht/component-library-css/dist/bem.css";
+import "@utrecht/design-tokens/dist/index.css";
+import "@utrecht/component-library-css/dist/index.css";
 import "@utrecht/component-library-css/dist/html.css";
-import "@utrecht/design-tokens/dist/theme/index.css";
 import "@gemeente-denhaag/design-tokens-components/dist/theme/index.css";
 import "@nl-design-system-unstable/amsterdam-design-tokens/dist/index.css";
 import "@utrecht/design-tokens/dist/index.css";
-import "@utrecht/component-library-css/dist/bem.css";
 import "../styles/amsterdam-theme.css";
 import "../styles/denhaag-theme.css";
 import "../styles/globals.css";
